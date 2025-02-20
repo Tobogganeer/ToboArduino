9ced536f493afb026c5838470362a763c503a78b
Evan D (Tobo) <66337562+Tobogganeer@users.noreply.github.com>
2025-02-20 10:46:07 -0500

    Space shooter keycard code

--- PresentationRemote/PresentationRemote.ino
+++ PresentationRemote/PresentationRemote.ino
@@ -1,37 +1,6 @@
 
 #include <Arduino.h>
 
-/*
- * Specify which protocol(s) should be used for decoding.
- * If no protocol is defined, all protocols (except Bang&Olufsen) are active.
- * This must be done before the #include <IRremote.hpp>
- */
-//#define DECODE_DENON        // Includes Sharp
-//#define DECODE_JVC
-//#define DECODE_KASEIKYO
-//#define DECODE_PANASONIC    // alias for DECODE_KASEIKYO
-//#define DECODE_LG
-//#define DECODE_NEC  // Includes Apple and Onkyo. To enable all protocols , just comment/disable this line.
-//#define DECODE_SAMSUNG
-//#define DECODE_SONY
-//#define DECODE_RC5
-//#define DECODE_RC6
-
-//#define DECODE_BOSEWAVE
-//#define DECODE_LEGO_PF
-//#define DECODE_MAGIQUEST
-//#define DECODE_WHYNTER
-//#define DECODE_FAST
-
-//#define DECODE_DISTANCE_WIDTH // Universal decoder for pulse distance width protocols
-//#define DECODE_HASH         // special decoder for all protocols
-
-//#define DECODE_BEO          // This protocol must always be enabled manually, i.e. it is NOT enabled if no protocol is defined. It prevents decoding of SONY!
-
-//#define DEBUG               // Activate this for lots of lovely debug output from the decoders.
-
-//#define RAW_BUFFER_LENGTH  750 // For air condition remotes it requires 750. Default is 200.
-
 #define IR_RECEIVE_PIN 2
 #include <IRremote.hpp>
 
---
+++ SpaceShooterKeycards/SpaceShooterKeycards.ino
@@ -0,0 +1,79 @@
+
+/*
+
+Code for a little rfid "keycard" reader for the Space Shooter project
+Uses example MFRC522 code
+
+Unused at the moment, may be later VVV
+------- my Pyduino library for serial communication
+-------https://github.com/Tobogganeer/Pyduino
+
+*/
+
+
+#include <SPI.h>
+#include <MFRC522.h>
+//#include <Pyduino.h>
+
+#define RST_PIN 9
+#define SS_PIN 10
+
+#define RESPONSE_BYTE_1 16
+#define RESPONSE_BYTE_2 32
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);  // RFID reader
+const int delayBetweenCards = 1000;
+
+void setup() {
+  //Pyduino::Init(); // Serial for Unity
+  //Pyduino::CustomMessageCallback = UnityMessage;
+  Serial.begin(115200);
+
+  while (!Serial); // Make sure serial port is opened
+
+  SPI.begin(); // SPI bus for rfid reader
+  mfrc522.PCD_Init();
+}
+
+void loop() {
+  //Pyduino::Tick(); // Check for any communications from Unity (will there even be any? Kinda a one way thing...)
+
+  // Wait if there is no card
+  if (!mfrc522.PICC_IsNewCardPresent() || !mfrc522.PICC_ReadCardSerial()) {
+    delay(50);
+    return;
+  }
+
+  // Send UID of card
+  byte response[6] = { RESPONSE_BYTE_1, RESPONSE_BYTE_2, 0, 0, 0, 0 };
+
+  //mfrc522.uid.size;
+  // Only read the first 4 bytes
+  for (byte i = 0; i < 4; i++) {
+    response[i + 2] = mfrc522.uid.uidByte[i];
+
+  // Send bytes
+  Serial.flush();
+  if (Serial.write(response, 6) != 6)
+    Serial.write(response, 6);
+  }
+
+  delay(delayBetweenCards);
+}
+
+
+long UnityMessage(byte* message, int len, int type) {
+  /*
+  if (type == messageIDImLookingFor)
+  {
+    int read = 0;
+    String str = Pyduino::GetString(message, read);
+    long num = Pyduino::GetInt(message, read);
+    
+    // Do something
+  }
+  */
+
+  // Return value sent to Unity
+  return 0;
+}
