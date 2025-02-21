81febe292981ed01f8f5dd28cb13138593a940a6
Evan D (Tobo) <66337562+Tobogganeer@users.noreply.github.com>
2025-02-20 20:29:19 -0500

    Successful audio output

--- BluetoothAudio/BluetoothAudio.ino
+++ BluetoothAudio/BluetoothAudio.ino
@@ -4,15 +4,19 @@
 btAudio audio = btAudio("Quandale Dingle's Whip");
 
 void setup() {
- 
- // Streams audio data to the ESP32   
- audio.begin();
- 
- // Re-connects to last connected device
- audio.reconnect();
 
+  // Streams audio data to the ESP32
+  audio.begin();
+
+  // Re-connects to last connected device
+  audio.reconnect();
+
+  int ws = 16;
+  int dout = 17;
+  int bck = 5;
+  audio.I2S(bck, dout, ws);
+  audio.volume(1.0);
 }
 
 void loop() {
-
 }
