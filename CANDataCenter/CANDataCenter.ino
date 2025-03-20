8bc0503320ed580dfd74a44367b8d615feb8a045
Evan D (Tobo) <66337562+Tobogganeer@users.noreply.github.com>
2025-03-20 13:07:06 -0400

    Reduced PCB photo size and started CAN center

---
+++ CANDataCenter/CANDataCenter.ino
@@ -0,0 +1,19 @@
+/*
+
+ESP8266 (D1 Mini clone)
+Gets data from CAN and LSCAN buses in car and transmits to other modules with ESP-NOW
+2010 Mazda 3
+
+Evan Daveikis
+
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
