8a83c3e7a11f54159a0a1a3dd360617db677f658
Tobogganeer <66337562+Tobogganeer@users.noreply.github.com>
2025-03-21 11:13:33 -0400

    Changed auto format for indent width and newline braces

--- BluetoothAudio/BluetoothAudio.ino
+++ BluetoothAudio/BluetoothAudio.ino
@@ -3,20 +3,21 @@
 // Sets the name of the audio device
 btAudio audio = btAudio("Quandale Dingle's Whip");
 
-void setup() {
+void setup()
+{
+    // Streams audio data to the ESP32
+    audio.begin();
 
-  // Streams audio data to the ESP32
-  audio.begin();
+    // Re-connects to last connected device
+    //audio.reconnect();
 
-  // Re-connects to last connected device
-  //audio.reconnect();
-
-  int ws = 16;
-  int dout = 17;
-  int bck = 5;
-  audio.I2S(bck, dout, ws);
-  audio.volume(1.0);
+    int ws = 16;
+    int dout = 17;
+    int bck = 5;
+    audio.I2S(bck, dout, ws);
+    audio.volume(1.0);
 }
 
-void loop() {
+void loop()
+{
 }
