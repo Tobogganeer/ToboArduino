8383a368cdcf1b409bd6b9f2e34ea66b72b69a63
Evan D (Tobo) <66337562+Tobogganeer@users.noreply.github.com>
2025-03-29 22:52:01 -0400

    Made test usable on Serial Plotter with Uno

--- CANDataLineTest/CANDataLineTest.ino
+++ CANDataLineTest/CANDataLineTest.ino
@@ -1,22 +1,24 @@
 
-// ESP32
+// Arduino UNO
 // Testing if CAN lines actually have data
 
-const int CAN_H = 34;
-const int CAN_L = 35;
+const int CAN_H = A0;
+const int CAN_L = A1;
 
-void setup() {
-  Serial.begin(115200);
-  delay(1000);
+void setup()
+{
+    Serial.begin(115200);
+    delay(1000);
 }
 
-void loop() {
-  // Reading potentiometer value
-  float canH = analogRead(CAN_H);
-  float canL = analogRead(CAN_L);
-  Serial.print("H=");
-  Serial.println(canH);
-  Serial.print("L=");
-  Serial.println(canL);
-  delay(10);
+void loop()
+{
+    // Reading potentiometer value
+    float canH = analogRead(CAN_H);
+    float canL = analogRead(CAN_L);
+    Serial.print("H:");
+    Serial.print(canH);
+    Serial.print(",L:");
+    Serial.println(canL);
+    delay(10);
 }
