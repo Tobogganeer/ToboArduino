af2c2d907f0e633b213b76ace643f5cf7266c354
Tobogganeer <66337562+Tobogganeer@users.noreply.github.com>
2025-04-26 13:38:09 -0400

    Tested it bt module can also fit display in memory (it can't)

--- BluetoothAudio/BluetoothAudio.ino
+++ BluetoothAudio/BluetoothAudio.ino
@@ -1,7 +1,13 @@
 #include <btAudio.h>
+#include <Wire.h>
+#include <U8g2lib.h>
 
 // Sets the name of the audio device
 btAudio audio = btAudio("Quandale Dingle's Whip");
+
+// Temp code just to test if all libs will fit into memory
+// https://www.youtube.com/watch?v=QixtxaAda18
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 
 void setup()
 {
@@ -16,6 +22,9 @@ void setup()
     int bck = 5;
     audio.I2S(bck, dout, ws);
     audio.volume(1.0);
+
+    // Temp
+    u8g2.begin();
 }
 
 void loop()
