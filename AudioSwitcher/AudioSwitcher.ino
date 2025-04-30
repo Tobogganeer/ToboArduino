ec73f06e5f0604831d203331f886ace79755b52d
Evan D (Tobo) <66337562+Tobogganeer@users.noreply.github.com>
2025-04-29 20:13:32 -0400

    Started planning out how audio switcher will work

--- AudioSwitcher/AudioSwitcher.ino
+++ AudioSwitcher/AudioSwitcher.ino
@@ -1,11 +1,52 @@
-/////////////////////////////////////////////////////////////////
+/*
+
+Audio Switcher
+
+Outline:
+- ESP8266 with a rotary encoder and 2 audio multiplexers (CD4052B)
+- Rotary encoder selects audio source
+- AUX multiplexer feeds into main multiplexer
+- Display audio source on OLED display
+- All outputs go through car's AUX port
+
+ESP8266 Wiring:
+
+D3 (GPIO0): Right side rotary encoder
+D4 (GPIO2): Left side rotary encoder
+GND: Middle rotary encoder
+
+D1 (GPIO5): SCL (I2C)
+D2 (GPIO4): SDA (I2C)
+
+D5 (GPIO14): Main audio select A
+D6 (GPIO12): Main audio select B
+D7 (GPIO13): AUX audio select A
+D8 (GPIO15): AUX audio select B
+
+Audio sources:
+ - 1: Bluetooth
+ - 2: AUX port
+ - 3: Lightning Cable
+ - 4: USB-C Cable
+ - 5: Carputer
+ - 6-8: Reserved
+
+Main audio select:
+ - Bluetooth
+ - AUX
+ - Carputer
+
+AUX audio select:
+ - AUX port
+ - Lightning
+ - USB-C
+
+*/
 
 #include "Rotary.h"
 
-/////////////////////////////////////////////////////////////////
-
-#define ROTARY_PIN1 5
-#define ROTARY_PIN2 4
+#define ROTARY_PIN1 2
+#define ROTARY_PIN2 0
 
 #define CLICKS_PER_STEP 4   // this number depends on your rotary encoder
 #define MIN_POS         4
