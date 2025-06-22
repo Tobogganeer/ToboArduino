cf415ffc11587e960ed8c4e7eb66f9cb4cd1f83e
Evan D (Tobo) <66337562+Tobogganeer@users.noreply.github.com>
2025-06-22 11:22:51 -0400

    Discovered what each button does (keypad is weird)

--- BluetoothPhone/BluetoothPhone.ino
+++ BluetoothPhone/BluetoothPhone.ino
@@ -12,6 +12,58 @@
  * LM - September 2019
  */
 
+
+
+/*
+
+Pin layout for phone keypad:
+
+1+5 = *
+1+7 = #
+1+8 = 0
+1+9 = flash
+2+5 = 7
+2+6 = store
+2+7 = 9
+2+8 = 8
+3+5 = 4
+3+6 = mem
+3+7 = 6
+3+8 = 5
+4+5 = 1
+4+6 = redial
+4+7 = 3
+4+8 = 2
+
+
+Pin 1: 
+Pin 2: 
+Pin 3: 
+Pin 4: 
+Pin 5: 
+Pin 6: 
+Pin 7: 
+
+
+
+=== Top buttons ===
+Row 1:
+Row 2:
+Row 3:
+Row 4:
+Row 5:
+=== Bottom Buttons ===
+
+Col 1:
+Col 2:
+Col 3:
+Col 4:
+=== Only for top row ===
+
+*/
+
+
+
 #include <Keypad.h>
 
 // Uncomment next line to enable serial console debugging.
@@ -22,11 +74,11 @@ const byte ROWS = 5;
 const byte COLS = 4;
 
 char keyMatrix[ROWS][COLS] = {
-  { 'R', 'M', 'S', 'F' },
-  { '1', '2', '3', 'A' },
-  { '4', '5', '6', 'B' },
-  { '7', '8', '9', 'C' },
-  { '*', '0', '#', 'D' }
+    { 'R', 'M', 'S', 'F' },
+    { '1', '2', '3', 'A' },
+    { '4', '5', '6', 'B' },
+    { '7', '8', '9', 'C' },
+    { '*', '0', '#', 'D' }
 };
 
 byte rowPins[ROWS] = { 19, 18, 5, 17, 16 };
@@ -53,61 +105,69 @@ Keypad myKeypad = Keypad(makeKeymap(keyMatrix), rowPins, colPins, ROWS, COLS);
 const long TONE_DURATION = 250;   // Milliseconds
 const long TEST_DURATION = 4000;  // For tweaking waveform
 
-void setup() {
+void setup()
+{
 #ifdef DEBUG
-  Serial.begin(DEBUG);
+    Serial.begin(DEBUG);
 #endif
-  ledcAttach(ROW_TONE_DIO, 0, PWM_RESOLUTION);
-  ledcAttach(COL_TONE_DIO, 0, PWM_RESOLUTION);
+    ledcAttach(ROW_TONE_DIO, 0, PWM_RESOLUTION);
+    ledcAttach(COL_TONE_DIO, 0, PWM_RESOLUTION);
 }
 
-void loop() {
-  char keyPressed = myKeypad.getKey();
+void loop()
+{
+    char keyPressed = myKeypad.getKey();
 
-  if (keyPressed) {
+    if (keyPressed)
+    {
 #ifdef DEBUG
-    Serial.print(keyPressed);
-    if (keyPressed == '#')
-      Serial.println();
+        Serial.print(keyPressed);
+        if (keyPressed == '#')
+            Serial.println();
 #endif
-    // Generate corresponding DTMF tone
-    dtmfON(keyPressed);
-    delay(TONE_DURATION);
-    dtmfOFF();
-  }
-}
-
-void dtmfOFF() {
-  ledcWriteTone(ROW_TONE_DIO, 0);
-  ledcWriteTone(COL_TONE_DIO, 0);
-  return;
-}
-
-void dtmfON(char c) {
-#ifdef DEBUG
-  Serial.println();
-  Serial.println("In dtmfON() - c = '" + String(c) + "'");
-#endif
-  int row = -1;
-  int col = -1;
-  for (int i = 0; i < ROWS; i++) {
-    for (int j = 0; j < COLS; j++) {
-      if (c == keyMatrix[i][j]) {
-        row = i;
-        col = j;
-        break;
-      }
+        // Generate corresponding DTMF tone
+        dtmfON(keyPressed);
+        delay(TONE_DURATION);
+        dtmfOFF();
     }
-    if (row >= 0)
-      break;
-  }
-  if (row < 0)  // Unencodeable character
+}
+
+void dtmfOFF()
+{
+    ledcWriteTone(ROW_TONE_DIO, 0);
+    ledcWriteTone(COL_TONE_DIO, 0);
     return;
-    // Row and column tones
+}
+
+void dtmfON(char c)
+{
 #ifdef DEBUG
-  Serial.print(" Row tone: " + String(dtmfRow[row]));
-  Serial.println("Hz,  Col tone: " + String(dtmfCol[col]) + "Hz");
+    Serial.println();
+    Serial.println("In dtmfON() - c = '" + String(c) + "'");
 #endif
-  ledcWriteTone(ROW_TONE_DIO, dtmfRow[row]);
-  ledcWriteTone(COL_TONE_DIO, dtmfCol[col]);
+    int row = -1;
+    int col = -1;
+    for (int i = 0; i < ROWS; i++)
+    {
+        for (int j = 0; j < COLS; j++)
+        {
+            if (c == keyMatrix[i][j])
+            {
+                row = i;
+                col = j;
+                break;
+            }
+        }
+        if (row >= 0)
+            break;
+    }
+    if (row < 0)  // Unencodeable character
+        return;
+        // Row and column tones
+#ifdef DEBUG
+    Serial.print(" Row tone: " + String(dtmfRow[row]));
+    Serial.println("Hz,  Col tone: " + String(dtmfCol[col]) + "Hz");
+#endif
+    ledcWriteTone(ROW_TONE_DIO, dtmfRow[row]);
+    ledcWriteTone(COL_TONE_DIO, dtmfCol[col]);
 }
