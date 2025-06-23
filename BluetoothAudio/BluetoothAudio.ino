9d73e8d974b6e3537a3528df39e282d2f3aef805
Evan D (Tobo) <66337562+Tobogganeer@users.noreply.github.com>
2025-06-23 07:58:56 -0400

    Check if BT audio can also have ESP-NOW (it can!)

--- BluetoothAudio/BluetoothAudio.ino
+++ BluetoothAudio/BluetoothAudio.ino
@@ -1,13 +1,11 @@
 #include <btAudio.h>
-#include <Wire.h>
-#include <U8g2lib.h>
+#include "CarDataReceiver.h"
 
 // Sets the name of the audio device
 btAudio audio = btAudio("Quandale Dingle's Whip");
 
 // Temp code just to test if all libs will fit into memory
 // https://www.youtube.com/watch?v=QixtxaAda18
-U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 
 void setup()
 {
@@ -23,9 +21,18 @@ void setup()
     audio.I2S(bck, dout, ws);
     audio.volume(1.0);
 
-    // Temp
-    u8g2.begin();
+    initCarDataReceiver(handleCarData);
 }
+
+
+void handleCarData(CarData* data)
+{
+    Serial.print("Got data. RPM: ");
+    Serial.print(data->rpm);
+    Serial.print(", Speed:");
+    Serial.println(data->speed);
+}
+
 
 void loop()
 {
--- libraries/CarDataReceiver/CarDataReceiver.h
+++ libraries/CarDataReceiver/CarDataReceiver.h
@@ -15,7 +15,7 @@ ESP8266/ESP32 modules
 // Most boards will be ESP8266 but the BT module is ESP32
 #ifndef ARDUINO_ARCH_ESP8266
 #include <esp_now.h>
-#include <esp_wifi.h>
+#include <WiFi.h>
 #else
 #include <ESP8266WiFi.h>
 #include <espnow.h>
@@ -26,7 +26,7 @@ CarData _CDR_internal_data;
 void (*_CDR_internal_callback) (CarData* data);
 
 #ifndef ARDUINO_ARCH_ESP8266
-void OnCarDataReceived(const uint8_t * mac, const uint8_t* incomingData, int len) {
+void OnCarDataReceived(const esp_now_recv_info* info, const uint8_t* incomingData, int len) {
 #else
 void OnCarDataReceived(uint8_t* mac, uint8_t* incomingData, uint8_t len) {
 #endif
@@ -48,7 +48,9 @@ void initCarDataReceiver(void (*dataCallback) (CarData* data))
 
     // Once ESPNow is successfully Init, we will register for recv CB to
     // get recv packer info
+    #ifdef ARDUINO_ARCH_ESP8266
     esp_now_set_self_role(ESP_NOW_ROLE_SLAVE);
+    #endif
     esp_now_register_recv_cb(OnCarDataReceived);
     _CDR_internal_callback = dataCallback;
 }
