86315d576af1cd02837cdf8d7344b0b0239da6aa
Evan D (Tobo) <66337562+Tobogganeer@users.noreply.github.com>
2025-10-23 00:23:11 -0400

    Test inputs for gear sensor

--- GearSensor/GearSensor.ino
+++ GearSensor/GearSensor.ino
@@ -1,11 +1,63 @@
+// 34, 35, 32, 33
+#include "CarComms.h"
+
+// LR is the short one
+// FB is the long one
+// L is close, R is far
+// F is close, B is far
+
+const int leftPin = 34;
+const int rightPin = 35;
+const int frontPin = 32;
+const int backPin = 33;
+
+CarComms comms(handleCarData);
+
 void setup()
 {
-    pinMode(A0, INPUT);
-    Serial.begin(9600);
+    Serial.begin(115200);
+
+    comms.begin();
+    comms.receiveTypeMask = 0;  // We don't want to receive at all, actually
+
+    pinMode(leftPin, INPUT);
+    pinMode(rightPin, INPUT);
+    pinMode(frontPin, INPUT);
+    pinMode(backPin, INPUT);
+}
+
+void handleCarData(CarDataType type, const uint8_t* data, int len)
+{
 }
 
 void loop()
 {
-    Serial.println(analogRead(A0));
-    delay(50);
+    // https://github.com/upiir/arduino_gear_indicator/blob/main/ARDUINO_gear_indicator/ARDUINO_gear_indicator.ino
+
+    float left = analogRead(leftPin) / 4095.0f;
+    float right = analogRead(rightPin) / 4095.0f;
+    float front = analogRead(frontPin) / 4095.0f;
+    float back = analogRead(backPin) / 4095.0f;
+
+    Serial.print("left:");
+    Serial.print(left);
+    Serial.print(",");
+
+    Serial.print("right:");
+    Serial.print(right);
+    Serial.print(",");
+
+    Serial.print("front:");
+    Serial.print(front);
+    Serial.print(",");
+
+    Serial.print("back:");
+    Serial.print(back);
+    Serial.println();
+
+    /*
+    GearMsg msg;
+    msg.gear = Gear::Neutral;
+    comms.send(CarDataType::ID_GEAR, &msg, sizeof(GearMsg));
+    */
 }
