ff53c45e1325f900b900cc9d47e21c5c24654391
Evan D (Tobo) <66337562+Tobogganeer@users.noreply.github.com>
2025-10-23 14:38:30 -0400

    Pinout for tank controller

---
+++ TankController/TankController.ino
@@ -0,0 +1,21 @@
+const int doorLeverPin = 3;
+const int fireButtonPin = 4;
+const int fireLEDPin = 5;
+const int enableAPin = 6;
+const int in2Pin = 7;
+const int in1Pin = 8;
+const int in3Pin = 9;
+const int in4Pin = 10;
+const int enableBPin = 11;
+const int echoPin = A6;
+const int triggerPin = A5;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
