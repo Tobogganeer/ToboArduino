fa2e05006897ae0901e05ac9faf42e8072830d9e
Tobogganeer <66337562+Tobogganeer@users.noreply.github.com>
2025-11-13 18:08:29 -0500

    Varying freq, duration, delay

--- Technomancy6Instrument/Technomancy6Instrument.ino
+++ Technomancy6Instrument/Technomancy6Instrument.ino
@@ -8,10 +8,10 @@ const byte ROWS = 4;
 const byte COLS = 4;
 
 char hexaKeys[ROWS][COLS] = {
-    { '1', '4', '7', '*' },
-    { '2', '5', '8', '0' },
-    { '3', '6', '9', '#' },
-    { 'A', 'B', 'C', 'D' }
+    { 1, 5, 9, 13 },
+    { 2, 6, 10, 14 },
+    { 3, 7, 11, 15 },
+    { 4, 8, 12, 16 }
 };
 byte rowPins[ROWS] = { 5, 4, 3, 2 };
 byte colPins[COLS] = { 9, 8, 7, 6 };
@@ -33,10 +33,30 @@ void loop()
     if (keypad.key[0].kstate == PRESSED)
         key = keypad.key[0].kchar;
 
-    int leftVal = analogRead(leftPot);
-    int rightVal = analogRead(rightPot);
+    // Wired them backwards oops
+    int leftVal = 1024 - analogRead(leftPot) + 1;
+    int rightVal = 1024 - analogRead(rightPot) + 1;
 
-    tone(buzzer, leftVal * 10, 20);
-    delay(25);
+    const int KeyMult = 10;
+    const int PotDivisor = 30;
+
+    const int BaseDuration = 15;
+    const int BaseDelay = 20;
+
+    const int DurationDivisor = 100;
+    const int DelayDivisor = 80;
+
+    const float Variation = 0.9f;
+
+    float time = millis() / (float)rightPot;
+    float loop = time % 1000;
+    float offset = (sin(loop) * Variation) + 1f;
+
+    int freq = (key * KeyMult) * (leftVal / PotDivisor);
+    int duration = BaseDuration * (rightVal / DurationDivisor);
+    int delayMS = BaseDelay * (rightVal / DelayDivisor);
+
+    tone(buzzer, freq, duration);
+    delay(delayMS);
     noTone(buzzer);
 }
