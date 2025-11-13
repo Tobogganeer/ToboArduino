6e1edadd9eda0ef40a88dc35c697b9065acea236
Tobogganeer <66337562+Tobogganeer@users.noreply.github.com>
2025-11-13 17:42:15 -0500

    Basic sound playing

--- Technomancy6Instrument/Technomancy6Instrument.ino
+++ Technomancy6Instrument/Technomancy6Instrument.ino
@@ -8,20 +8,35 @@ const byte ROWS = 4;
 const byte COLS = 4;
 
 char hexaKeys[ROWS][COLS] = {
-  { '1', '4', '7', '*' },
-  { '2', '5', '8', '0' },
-  { '3', '6', '9', '#' },
-  { 'A', 'B', 'C', 'D' }
+    { '1', '4', '7', '*' },
+    { '2', '5', '8', '0' },
+    { '3', '6', '9', '#' },
+    { 'A', 'B', 'C', 'D' }
 };
 byte rowPins[ROWS] = { 5, 4, 3, 2 };
 byte colPins[COLS] = { 9, 8, 7, 6 };
 
-Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
+Keypad keypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
 
-void setup() {
-  // put your setup code here, to run once:
+void setup()
+{
+    pinMode(buzzer, OUTPUT);
+    pinMode(leftPot, INPUT);
+    pinMode(rightPot, INPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+    char key = 0;
+    keypad.getKeys();
+
+    if (keypad.key[0].kstate == PRESSED)
+        key = keypad.key[0].kchar;
+
+    int leftVal = analogRead(leftPot);
+    int rightVal = analogRead(rightPot);
+
+    tone(buzzer, leftVal * 10, 20);
+    delay(25);
+    noTone(buzzer);
 }
